036a2c3f5ba04dc86dc118082471e037a3958955
kitamas <kitamas25@gmail.com>
2022-09-28 08:08:07 +0200

    commit

--- delete.py
+++ delete.py
@@ -89,23 +89,19 @@ def webhook():
 def main():
 
     req = request.get_json(force=True)
-    print(json.dumps(req, indent=4))
-    #event_id = req.get('sessionInfo').get('parameters').get('event_id')
-    event_id = "AAA"
-    print("EVENT ID")
-    print(event_id)
+    #print(json.dumps(req, indent=4))
+    event_id = req.get('sessionInfo').get('parameters').get('event_id')
 
     creds = authentication()
     service = build("calendar", "v3", credentials=creds)
 
-    service.events().delete(calendarId='primary', eventId='eventId').execute()
-    #service.events().delete(calendarId='primary', eventId=event_id).execute()
+    #service.events().delete(calendarId='primary', eventId='eventId').execute()
+    service.events().delete(calendarId='primary', eventId=event_id).execute()
 
     text = "id: " + event_id + "event deleted"
     text_param = {}
     text_param['text'] = text
-    #text_param['event_id'] = event_result['id']
-    text_param['event_id'] = event_id
+    text_param['event_id'] = event_result['id']
   
     #return text
     return text_param
